49c53a70ed1449b78533ff2f9b3e09e6c470f462
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-11-17 20:30:32 +1100

    Projectile Trail is the last thing

---
+++ Assets/Particle Systems/Spell/Healing/Scripts/HealingControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class HealingControl : MonoBehaviour
+{
+    public GameObject baseParticle;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            animator.SetBool("IsPlaying", true);
+        }
+    }
+}
---
+++ Assets/Particle Systems/Spell/Scripts/ParticleCycle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class ParticleCycle : MonoBehaviour
+{
+    public Transform spawnPosition;
+    public GameObject particle;
+    public float existenceTime;
+
+    private float _firstExistence = 0;
+    private GameObject _particleClone;
+
+    private void Start()
+    {
+        _particleClone = Instantiate(particle, spawnPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.deltaTime > _firstExistence)
+        {
+            _firstExistence = Time.deltaTime + existenceTime;
+            _particleClone.SetActive(false);
+        }
+        else
+        {
+            _particleClone.SetActive(true);
+        }
+    }
+}
